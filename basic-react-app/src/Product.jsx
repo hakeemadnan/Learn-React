412d74e84e1514955519590cc95bc49bcda8ea0a
hakeemadnan <hakeemadnan86@gmail.com>
2026-05-19 13:59:15 +0530

    component styling

--- basic-react-app/src/Product.jsx
+++ basic-react-app/src/Product.jsx
@@ -2,14 +2,16 @@ import "./Product.css";
 import Price from "./price";
 function Product({title,idx}) {
 
-    
+
     let oldPrices = ["12,495","11,900","1,599","599"]
     let newPrices = ["9595","8900","899","299"]
-    let Description = ["8000 DPI","intuitive surface","designed for ipad pro","wireless"]
+    let Description = [["8000 DPI","8000 DPI"],["intuitive surface","intuitive surface"],["designed for ipad pro","intuitive surface"],["wireless","intuitive surface"]]
     return (
     <div className="Product">
       <h4>{title}</h4>
-      <p>{Description[idx]}</p>
+      <p>{Description[idx][0]}</p>
+      
+      <p>{Description[idx][1]}</p>
       <Price oldPrice={oldPrices[idx]} newPrice={newPrices[idx]}/>
     </div>
   );
--- basic-react-app/src/price.jsx
+++ basic-react-app/src/price.jsx
@@ -1,21 +1,22 @@
-export default function Price({oldPrice,newPrice}){
-    let oldstyles ={
-        textDecorationLine :"line-through",
-    };
-    let newStyles = {
-        fontWeight: "bold,"
-    };
-    let styles ={
-        backgroundColor : "#e0c367",
-        height:"30px",
-        borderBottomLeftRadius : "14px",
-        borderBottomRightRadius: "14px",
-    }
-    return (
-        <div style={styles}>
-            <span style={oldstyles}>{oldPrice}</span>
-            &nbsp;
-            <span style={newStyles}>{newPrice}</span>
-        </div>
-    )
+export default function Price({ oldPrice, newPrice }) {
+  let oldstyles = {
+    textDecorationLine: "line-through",
+  };
+  let newStyles = {
+    fontWeight: "bold,",
+  };
+  let styles = {
+    backgroundColor: "#e0c367",
+    height: "30px",
+    width: "200px",
+    borderBottomLeftRadius: "23px",
+    borderBottomRightRadius: "23px",
+  };
+  return (
+    <div style={styles}>
+      <span style={oldstyles}>{oldPrice}</span>
+      &nbsp;
+      <span style={newStyles}>{newPrice}</span>
+    </div>
+  );
 }
